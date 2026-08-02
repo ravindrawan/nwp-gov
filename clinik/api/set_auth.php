b16099eea4660ae17a765cb2cec6ea3881c0dba1
ravindrawan <132208871+ravindrawan@users.noreply.github.com>
2026-08-02 14:42:14 +0530

    Update set_auth.php

--- clinik/api/set_auth.php
+++ clinik/api/set_auth.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
+header('Content-Type: application/json');
 
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (isset($data['password']) && $data['password'] === 'Ravi@2026') {
+    $_SESSION['admin_logged_in'] = true;
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['success' => false, 'message' => 'Invalid password']);
+}
