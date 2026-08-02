75d5008cffa98d49b6687b5d7e748f81cc15afcf
ravindrawan <132208871+ravindrawan@users.noreply.github.com>
2026-08-02 14:40:49 +0530

    Update admin.php

--- clinik/admin.php
+++ clinik/admin.php
@@ -1,4 +1,13 @@
 <?php
+
+
+session_start();
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header("Location: index.php");
+    exit();
+}
+
+
 /**
  * Admin Management Console - Tuesday Drop HQ
  */
